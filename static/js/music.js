b8658daaaf97029d3efe8b3b75cd8f787826dbed
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-07 22:28:05 +0700

    tambahan pada js

--- static/js/music.js
+++ static/js/music.js
@@ -1,5 +1,12 @@
-const music = new Audio('../songs/Spine.mp3');
-music.play();
+const music = new Audio('./static/songs/Spine.mp3');
+
+const songs = [
+    {
+        id: 1,
+        songName: 'Spine Breaker<br> <div class="subtitle"> Skool Luv Affair Special Addition</div>',
+        poster: "img/spine.jpg"
+    }
+]
 
 let pop_song_left = document.getElementById('pop_song_left');
 let pop_song_right = document.getElementById('pop_song_right');
