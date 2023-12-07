7b2bf9ca032a9b87ba840eec5ea1135e08c4d9bb
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-07 16:15:10 +0700

    perubahan

--- static/js/mymusic.js
+++ static/js/mymusic.js
@@ -6,20 +6,6 @@ let pop_song = document.getElementsByClassName('pop_song')[0];
 pop_song_right.addEventListener('click', () => {
     pop_song.scrollLeft += 330;
 });
-
 pop_song_left.addEventListener('click', () => {
     pop_song.scrollLeft -= 330;
-});
-
-let pop_art_left = document.getElementById('pop_art_left');
-let pop_art_right = document.getElementById('pop_art_right');
-let Artist_bx = document.getElementsByClassName('Artist_bx')[0];
-
-
-pop_art_right.addEventListener('click', () => {
-    Artist_bx.scrollLeft += 330;
-});
-
-pop_art_left.addEventListener('click', () => {
-    Artist_bx.scrollLeft -= 330;
 });
