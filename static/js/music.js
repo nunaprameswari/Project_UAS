d77bd9687cd8e3c78a83ec24bb3168cb70ae0164
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-07 22:49:00 +0700

    megalami perubahan

--- static/js/music.js
+++ static/js/music.js
@@ -7,49 +7,49 @@ const songs = [
         poster: "img/spine.jpg"
     },
     {
-        id: 1,
-        songName: 'Spine Breaker<br> <div class="subtitle"> Skool Luv Affair Special Addition</div>',
-        poster: "img/spine.jpg"
+        id: 2,
+        songName: 'Danger <br> <div class="subtitle"> Wake Up</div>',
+        poster: "img/danger.jpg"
     },
     {
-        id: 1,
-        songName: 'Spine Breaker<br> <div class="subtitle"> Skool Luv Affair Special Addition</div>',
-        poster: "img/spine.jpg"
+        id: 3,
+        songName: 'Dynamite <br> <div class="subtitle"> Be</div>',
+        poster: "img/Dynamite.jpg"
     },
     {
-        id: 1,
-        songName: 'Spine Breaker<br> <div class="subtitle"> Skool Luv Affair Special Addition</div>',
-        poster: "img/spine.jpg"
+        id: 4,
+        songName: 'IDOL <br> <div class="subtitle"> Love Yourself: Answer </div>',
+        poster: "img/idol.jpg"
     },
     {
-        id: 1,
-        songName: 'Spine Breaker<br> <div class="subtitle"> Skool Luv Affair Special Addition</div>',
-        poster: "img/spine.jpg"
+        id: 5,
+        songName: 'Louder than bombs <br> <div class="subtitle"> Map of the Soul: 7 </div>',
+        poster: "img/louder than bombs.jpg"
     },
     {
-        id: 1,
-        songName: 'Spine Breaker<br> <div class="subtitle"> Skool Luv Affair Special Addition</div>',
-        poster: "img/spine.jpg"
+        id: 6,
+        songName: 'MIC Drop <br> <div class="subtitle"> Love Yourself: Her </div>',
+        poster: "img/mic drop.jpg"
     },
     {
-        id: 1,
-        songName: 'Spine Breaker<br> <div class="subtitle"> Skool Luv Affair Special Addition</div>',
-        poster: "img/spine.jpg"
+        id: 7,
+        songName: 'RUN <br> <div class="subtitle"> Youth</div>',
+        poster: "img/run.jpg"
     },
     {
-        id: 1,
-        songName: 'Spine Breaker<br> <div class="subtitle"> Skool Luv Affair Special Addition</div>',
-        poster: "img/spine.jpg"
+        id: 8,
+        songName: 'Spring Day <br> <div class="subtitle"> You Never Walk Alone</div>',
+        poster: "img/spring day.jpg"
     },
     {
-        id: 1,
-        songName: 'Spine Breaker <br> <div class="subtitle"> Skool Luv Affair Special Addition</div>',
-        poster: "img/spine.jpg"
+        id: 9,
+        songName: '00:00 (Zero O`Clock) <br> <div class="subtitle"> Map of the Soul: </div>',
+        poster: "img/zero.jpg"
     },
     {
-        id: 1,
-        songName: 'Spine Breaker<br> <div class="subtitle"> Skool Luv Affair Special Addition</div>',
-        poster: "img/spine.jpg"
+        id: 10,
+        songName: 'FIRE <br> <div class="subtitle"> The Most Beatiful Moment In Life: Young Forever </div>',
+        poster: "img/fire.jpg"
     }
 ]
 
