d614b942d2fc97143a697f6f9eebdcb27a361e17
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-07 23:10:15 +0700

    megalami perubahan dan tambahan

--- static/js/music.js
+++ static/js/music.js
@@ -4,54 +4,63 @@ const songs = [
     {
         id: 1,
         songName: 'Spine Breaker<br> <div class="subtitle"> Skool Luv Affair Special Addition</div>',
-        poster: "img/spine.jpg"
+        poster: "./static/img/spine.jpg"
     },
     {
         id: 2,
         songName: 'Danger <br> <div class="subtitle"> Wake Up</div>',
-        poster: "img/danger.jpg"
+        poster: "./static/img/danger.jpg"
     },
     {
         id: 3,
         songName: 'Dynamite <br> <div class="subtitle"> Be</div>',
-        poster: "img/Dynamite.jpg"
+        poster: "./static/img/Dynamite.png"
     },
     {
         id: 4,
         songName: 'IDOL <br> <div class="subtitle"> Love Yourself: Answer </div>',
-        poster: "img/idol.jpg"
+        poster: "./static/img/idol.jpg"
     },
     {
         id: 5,
         songName: 'Louder than bombs <br> <div class="subtitle"> Map of the Soul: 7 </div>',
-        poster: "img/louder than bombs.jpg"
+        poster: "./static/img/louder than bombs.jpg"
     },
     {
         id: 6,
         songName: 'MIC Drop <br> <div class="subtitle"> Love Yourself: Her </div>',
-        poster: "img/mic drop.jpg"
+        poster: "./static/img/mic drop.jpg"
     },
     {
         id: 7,
         songName: 'RUN <br> <div class="subtitle"> Youth</div>',
-        poster: "img/run.jpg"
+        poster: "./static/img/run.jpg"
     },
     {
         id: 8,
         songName: 'Spring Day <br> <div class="subtitle"> You Never Walk Alone</div>',
-        poster: "img/spring day.jpg"
+        poster: "./static/img/spring day.jpg"
     },
     {
         id: 9,
         songName: '00:00 (Zero O`Clock) <br> <div class="subtitle"> Map of the Soul: </div>',
-        poster: "img/zero.jpg"
+        poster: "./static/img/zero.jpg"
     },
     {
         id: 10,
         songName: 'FIRE <br> <div class="subtitle"> The Most Beatiful Moment In Life: Young Forever </div>',
-        poster: "img/fire.jpg"
+        poster: "./static/img/fire.jpg"
+    },
+    {
+        id: 11,
+        songName: 'RUN <br> <div class="subtitle"> Youth </div>',
+        poster: "./static/img/run.jpg"
     }
 ]
+
+Array.from(document.getElementsByClassName('songItem')).forEach((e, i) => {
+    e.getElementsByTagName('img')[0].src = songs[i].poster;
+})
 
 let pop_song_left = document.getElementById('pop_song_left');
 let pop_song_right = document.getElementById('pop_song_right');
