9d1e150d2a3a46d74d819645bf8d5038ea1becb8
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-10 11:16:52 +0700

    mengalami perubahan

--- Army/views.py
+++ Army/views.py
@@ -10,3 +10,7 @@ def menu(request):
 def ArmyShop(request):
     template = loader.get_template('ArmyShop.html')
     return HttpResponse(template.render())
+
+def MyFavorite(request):
+    template = loader.get_template('MyFavorite.html')
+    return HttpResponse(template.render())
