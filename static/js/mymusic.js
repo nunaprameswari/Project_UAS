d8409b84876158332dc8b6ef80735deeccb99351
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-13 20:21:51 +0700

    editing file js

--- static/js/mymusic.js
+++ static/js/mymusic.js
@@ -5,6 +5,30 @@ const songs = [
         <div class="subtitle">Love Yourself 轉 Tear (2018)</div>`,
         poster: "./static/img/anpanman1.jpg",
     },
+    {
+        id: '2',
+        songName: `뱁새 (Baepsae) <br>
+        <div class="subtitle">In The Mood For Love pt.2 (2016)</div>`,
+        poster: "./static/img/baepsae1.jpg",
+    },
+    {
+        id: '3',
+        songName: `Black Swan <br>
+        <div class="subtitle">Map Of The Soul 7 (2020)`,
+        poster: "./static/img/black_swan1.jpg",
+    },
+    {
+        id: '4',
+        songName: `Danger -Japanese Ver.- <br>
+        <div class="subtitle">Danger [Japanese]</div>`,
+        poster: "./static/img/danger1.png",
+    },
+    {
+        id: '5',
+        songName: `Ddaeng <br>
+        <div class="subtitle">DD Ceremony</div>`,
+        poster: "./static/img/ddaeng1.jpg",
+    },
 ]
 
 
