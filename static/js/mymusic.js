3b9d0a7ef7a67c4be6b8f4f687bba2a91ae52839
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-14 14:18:56 +0700

    pengeditan

--- static/js/mymusic.js
+++ static/js/mymusic.js
@@ -1,3 +1,5 @@
+const music = new Audio('songs/5.mp3');
+
 const songs = [
     {
         id: '1',
@@ -180,11 +182,12 @@ const songs = [
 
 let pop_song_left = document.getElementById('pop_song_left');
 let pop_song_right = document.getElementById('pop_song_right');
-let pop_song = document.getElementsByClassName('pop_sonng')[0];
+let pop_song = document.getElementsByClassName('pop_song')[0];
 
 
 pop_song_right.addEventListener('click', () => {
     pop_song.scrollLeft += 330;
+    console.log('hi');
 });
 pop_song_left.addEventListener('click', () => {
     pop_song.scrollLeft -= 330;
